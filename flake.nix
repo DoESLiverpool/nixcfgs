c411c19c3a8b4d5571c64bf80c0f95085063f651
matthewcroughan <matt@croughan.sh>
2021-06-10 04:45:39 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "A NixOS flake for the Astralship and the machines aboard it.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations = {
+      vinylcutter = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (import ./hosts/vinylcutter/configuration.nix)
+          (import ./mixins/nix2.4.nix)
+        ];
+        specialArgs = { inherit inputs; };
+      };
+    };
+  };
+}
+
---
+++ mixins/nix2.4.nix
@@ -0,0 +1,12 @@
+# Makes machines use nix2.4 if imported.
+
+{ pkgs, ... }:
+
+{
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+}
