b97e99820b6fe4731f4ccecbb30abce0bc0d10fb
matthewcroughan <matt@croughan.sh>
2021-06-18 02:52:31 +0100

    profiles: init

---
+++ profiles/wireless.nix
@@ -0,0 +1,8 @@
+{
+  networking.wireless = {
+    enable = true;
+    networks = { 
+      DoESLiverpool.pskRaw = "63e49f779a41eda7be1510a275a07e519d407af706d0f2d3cc3140b9aecd412f"; 
+    };
+  };
+}
