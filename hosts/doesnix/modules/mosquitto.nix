d1d9435032becd342cea87f0139c7f910d78dfa2
matthewcroughan <matt@croughan.sh>
2021-11-22 17:41:00 +0000

    mosquitto: add note to mosquitto.nix
    Adds a comment to remind of how the configuration should look when using NixOS 21.11

--- hosts/doesnix/modules/mosquitto.nix
+++ hosts/doesnix/modules/mosquitto.nix
@@ -10,3 +10,24 @@
     '';
   };
 }
+
+# When we upgrade to NixOS 21.11, the config should look like this.
+#
+# {
+#   networking.firewall.allowedTCPPorts = [ 1883 ];
+#   services.mosquitto = {
+#     enable = true;
+#     settings.max_keepalive = 60;
+#     listeners = [
+#       {
+#         port = 1883;
+#         omitPasswordAuth = true;
+#         users = {};
+#         settings = {
+#           allow_anonymous = true;
+#         };
+#         acl = [ "topic readwrite #" ];
+#       }
+#     ];
+#   };
+# }
