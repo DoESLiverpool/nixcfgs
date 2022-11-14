19b5115f8d5053e76d7e8f70e221960c872a4b64
matthewcroughan <matt@croughan.sh>
2022-11-14 17:33:17 +0000

    Add nixinate to the machines
    Now anyone with keys can nix run .#apps.nixinate.<machineName> and
    deploy changes to a machine

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   description = "A NixOS flake for the DoESLiverpool makerspace and the machines within it.";
 
   inputs = {
+    nixinate.url = "github:matthewcroughan/nixinate";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     home-manager.url = "github:nix-community/home-manager";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-master, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, nixpkgs-master, home-manager, nixinate, ... }@inputs: {
+    apps = nixinate.nixinate.x86_64-linux self;
     nixosConfigurations = {
       vinylcutter = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -20,6 +22,13 @@
             home-manager.useUserPackages = true;
             home-manager.users.doesuser = { home.username = "doesuser"; home.homeDirectory = "/home/doesuser"; };
           }
+          {
+            _module.args.nixinate = {
+              host = "vinylcutter.local";
+              sshUser = "doesuser";
+              buildOn = "remote"; # valid args are "local" or "remote"
+            };
+          }
         ];
         specialArgs = { inherit inputs; };
       };
@@ -28,6 +37,13 @@
         modules = [
           (import ./hosts/doesnix/configuration.nix)
           (import ./mixins/nix2.4.nix)
+          {
+            _module.args.nixinate = {
+              host = "doesnix.local";
+              sshUser = "matthew";
+              buildOn = "remote"; # valid args are "local" or "remote"
+            };
+          }
         ];
         specialArgs = { inherit inputs; };
       };
